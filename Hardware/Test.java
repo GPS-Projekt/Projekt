f71e56e2a0f60b9a276d5303388606d4d4425381
TDiedr <84705831+TDiedr@users.noreply.github.com>
2021-07-25 18:53:47 +0200

    Add files via upload
    Rewritten read method and added setup method. Way to differentiate the correct commport as well as refined output still missing. Old read method left as comment for the time being will be taken out in the next version.

--- Hardware/Test.java
+++ Hardware/Test.java
@@ -1,6 +1,10 @@
 package Hardware;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
@@ -25,6 +29,16 @@ public class Test {
 		return portNameList;
 	}
 	
+	public SerialPort setupPort(String s) throws PortInUseException, NoSuchPortException, UnsupportedCommOperationException{
+		
+		CommPortIdentifier pID = CommPortIdentifier.getPortIdentifier(s);
+		SerialPort port = (SerialPort) pID.open(s,0);
+		port.setSerialPortParams(4800,SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+	
+		return port;
+	}
+	
+	/*
 	public String readFromPort(SerialPort port) {
 		
 		//reads from the given serialport 
@@ -41,12 +55,14 @@ public class Test {
 			inputStream = port.getInputStream();
 			
 			while((byte)inputStream.read() !='$') {
-				if((byte) inputStream.read()=='$') break;
+				if((byte) inputStream.read()=='$') {
+					do {
+						byteList.add((byte)inputStream.read());
+					}while((byte) inputStream.read() != '$');
+				};
 			};
 			
-			do {
-				byteList.add((byte)inputStream.read());
-			}while((byte) inputStream.read() != '$');
+
 			
 			Byte[] arr = byteList.toArray(new Byte[0]);
 			byte[] bytes = new byte[arr.length];
@@ -54,7 +70,7 @@ public class Test {
 			for(Byte b:arr)
 				bytes[i++] = b.byteValue();
 			
-			String s = new String(bytes);
+			String s = new String(bytes, "UTF-8");
 			output=s;
 			
 		} catch (IOException e) {
@@ -63,6 +79,34 @@ public class Test {
 
 		return output;
 	}
+	*/
+	
+	public String readFromPort(SerialPort port) {
+		
+		//reworked read method with external input due to lacking knowledge regarding streams
+		//first tests with buffered reader had issues converting the byte output to string
+		//ByteArrayOutputStream fixed that issue
+		
+		InputStream is;
+		try {
+			is = port.getInputStream();
+			BufferedInputStream bis = new BufferedInputStream(is);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			
+			for(int result = bis.read(); result != -1; result = bis.read()) {
+				bos.write((byte) result);
+			}
+		
+			return bos.toString("UTF-8");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String worstcase = "";
+		return worstcase;
+		
+	}
 	
 	public static void main(String[]args) {
 		
@@ -70,19 +114,24 @@ public class Test {
 		
 		System.out.println(t.getPorts());
 		
-		CommPortIdentifier portIdentifier;
-		try {
-			portIdentifier = CommPortIdentifier.getPortIdentifier("COM3");
-			SerialPort port3 = (SerialPort) portIdentifier.open("COM3", 0);
-			// might have to configure parameters still
-			while(true) {
-				System.out.println(t.readFromPort(port3));
+
+			SerialPort port3;
+			try {
+				port3 = t.setupPort("COM3");
+
+				while(true) {
+					System.out.println(t.readFromPort(port3));
+				}
+			} catch (PortInUseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchPortException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedCommOperationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (NoSuchPortException e) {
-			e.printStackTrace();
-		} catch (PortInUseException e) {
-			e.printStackTrace();
-		}
 		
 	}
 }
